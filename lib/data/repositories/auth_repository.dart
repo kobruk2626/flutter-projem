f5c284cfceba77d241c0916235832903ab24927a
kobruk2626 <obrukkaan@gmail.com>
2025-11-07 19:34:46 +0300

    Delete lib directory

--- lib/data/repositories/auth_repository.dart
+++
@@ -1,9 +0,0 @@
-abstract class AuthRepository {
-  Future<UserModel?> getCurrentUser();
-  Future<UserModel> signInWithEmailAndPassword(String email, String password);
-  Future<UserModel> registerWithEmailAndPassword(String email, String password, String name, String? phone);
-  Future<void> signOut();
-  Future<void> resetPassword(String email);
-  Future<void> updateUserProfile(UserModel user);
-  Stream<UserModel?> get userStream;
-}
